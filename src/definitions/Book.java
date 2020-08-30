66407ebdbe26053f83cc19e68cb74f2e3f8fb749
This PC <khushi.agarwal_cs19@gla.ac.in>
2020-08-30 08:09:04 +0530

    create private fields in Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,10 +1,15 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:49 PM
+ *  User: Khushi Agarwal (khushia1721)
+ *  Date: 30/08/2020
  *  File Name : Book.java
  * */
 package definitions;
 
 public class Book {
+
+    private String bookName;
+    private String authorName;
+    private String ISBNNumber;
+
+
 }
