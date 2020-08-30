d20e6bebe77a175f621c68fbe8979ba5236264fc
This PC <khushi.agarwal_cs19@gla.ac.in>
2020-08-30 08:45:58 +0530

    create getter and setter method in Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -8,4 +8,14 @@ package definitions;
 public class Library {
 
     private Book[] booksThatAreCurrentlyAvailable;
+
+    public Book[] getBooksThatAreCurrentlyAvailable() {
+        return booksThatAreCurrentlyAvailable;
+    }
+
+    public void setBooksThatAreCurrentlyAvailable(Book[] booksThatAreCurrentlyAvailable) {
+        this.booksThatAreCurrentlyAvailable = booksThatAreCurrentlyAvailable;
+    }
+
+
 }
