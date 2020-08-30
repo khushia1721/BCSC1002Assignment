51e7ee05c87acdc900b304d7c842abfe0d236279
This PC <khushi.agarwal_cs19@gla.ac.in>
2020-08-30 08:20:37 +0530

    create private fields in Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -1,10 +1,18 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:49 PM
+ *  User: Khushi Agarwal (khushia1721)
+ *  Date: 30/08/20
  *  File Name : Student.java
  * */
 package definitions;
 
 public class Student {
+
+    private String firstNameOfTheStudent;
+    private String middleNameOfTheStudent;
+    private String lastNameOfTheStudent;
+    private long universityRollNumberOfTheStudent;
+    private int numberOfBooksIssuedByTheStudent;
+    private Book[] namesOfTheBooksIssuedByTheStudent;
+
+
 }
