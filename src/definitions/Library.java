5fe3ea130546a7d66c7f0646ba7d2357e2b84e7f
This PC <khushi.agarwal_cs19@gla.ac.in>
2020-08-30 08:45:15 +0530

    create private field in Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -1,10 +1,11 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:50 PM
+ *  User: Khushi Agarwal (khushia1721)
+ *  Date: 30/08/20
  *  File Name : Library.java
  * */
 package definitions;
 
 public class Library {
+
+    private Book[] booksThatAreCurrentlyAvailable;
 }
