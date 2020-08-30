135bed8f7413cca531da39488b5dd96cb50a8fc8
This PC <khushi.agarwal_cs19@gla.ac.in>
2020-08-30 09:37:41 +0530

    create else part of FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,6 +6,11 @@
  * */
 package execution;
 
+import definitions.Library;
+import definitions.Student;
+
+import java.util.Scanner;
+
 public class FrontDesk {
 
     private static final int ISSUE_A_NEW_BOOK = 1;
@@ -14,13 +19,62 @@ public class FrontDesk {
     private static final int EXIT = 4;
 
     public static void main(String[] args) {
-        System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-");
-        System.out.println("How may I help you today?");
-        System.out.println("1. Issue a new book for me.");
-        System.out.println("2. Return a previously issues book for me.");
-        System.out.println("3. Show me all my issues books.");
-        System.out.println("4. Exit.");
-        System.out.println("Enter your choice (1..4): ");
-
+        Scanner scanner = new Scanner(System.in);
+        int studentChoice;
+        Student student = new Student();
+        Library library = new Library();
+        library.setBooksThatAreCurrentlyAvailable();
+        String bookName;
+        do {
+            System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-");
+            System.out.println("How may I help you today?");
+            System.out.println("1. Issue a new book for me.");
+            System.out.println("2. Return a previously issues book for me.");
+            System.out.println("3. Show me all my issues books.");
+            System.out.println("4. Exit.");
+            System.out.println("Enter your choice (1..4): ");
+            studentChoice = scanner.nextInt();
+            switch (studentChoice) {
+                case ISSUE_A_NEW_BOOK:
+                    scanner.nextLine();
+                    System.out.println("Enter your name: ");
+                    student.setFirstNameOfTheStudent(scanner.nextLine());
+                    student.setMiddleNameOfTheStudent(scanner.nextLine());
+                    student.setLastNameOfTheStudent(scanner.nextLine());
+                    System.out.println("Enter your University roll number: ");
+                    student.setUniversityRollNumberOfTheStudent(scanner.nextLong());
+                    System.out.println("How much books you want to issue: ");
+                    student.setNumberOfBooksIssuedByStudent(scanner.nextInt());
+                    scanner.nextLine();
+                    System.out.println("Enter the details of the books you want to issue: ");
+                    for (int bookIssuingIndex = 0; bookIssuingIndex <
+                            student.getNumberOfBooksIssuedByStudent(); bookIssuingIndex++) {
+                        System.out.println("Enter name of book " + (bookIssuingIndex + 1) + ": ");
+                        bookName = scanner.nextLine();
+                        student.getNamesOfBooksIssuedByStudent();
+                    }
+                    System.out.println("Thank you for Issuing the Books..");
+                    break;
+                case RETURN_A_PREVIOUSLY_ISSUED_BOOK:
+                    if (student.getNumberOfBooksIssuedByStudent() == 0) {
+                        System.out.println("Sorry, you haven't issued any book yet so you can't return book.");
+                    } else {
+                        System.out.println("Enter the name of the book you want to return: ");
+                        scanner.nextLine();
+                        bookName = scanner.nextLine();
+                    }
+                    break;
+                case SHOW_ALL_ISSUED_BOOKS:
+                    student.showIssuedBooksByStudent();
+                    break;
+                case EXIT:
+                    System.out.println("Thanks for using our service.");
+                    break;
+                default:
+                    System.out.println("Your have entered wrong choice!");
+                    break;
+            }
+        } while (studentChoice != EXIT);
+        scanner.close();
     }
 }
