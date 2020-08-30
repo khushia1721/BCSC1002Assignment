c5944480c666c3e832b9b7f8f42293f541002e98
This PC <khushi.agarwal_cs19@gla.ac.in>
2020-08-30 09:02:33 +0530

    create private fields in FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -7,6 +7,12 @@
 package execution;
 
 public class FrontDesk {
+
+    private static final int ISSUE_A_NEW_BOOK = 1;
+    private static final int RETURN_A_PREVIOUSLY_ISSUED_BOOK = 2;
+    private static final int SHOW_ALL_ISSUED_BOOKS = 3;
+    private static final int EXIT = 4;
+
     public static void main(String[] args) {
 
     }
