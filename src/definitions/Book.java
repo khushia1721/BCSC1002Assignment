49a291f8fd6050ab6b1f4d226251a77ab61abcdc
This PC <khushi.agarwal_cs19@gla.ac.in>
2020-08-30 08:10:47 +0530

    create setter methods in Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -15,13 +15,24 @@ public class Book {
         return bookName;
     }
 
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
     public String getAuthorName() {
         return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
     }
 
     public String getISBNNumber() {
         return ISBNNumber;
     }
 
+    public void setISBNNumber(String ISBNNumber) {
+        this.ISBNNumber = ISBNNumber;
+    }
 
 }
